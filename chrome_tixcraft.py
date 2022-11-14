067d4e01c8284fc26a3095250542711ab5b0b59e
CHUN YU YAO <weng.32002@gmail.com>
2022-11-14 10:08:13 +0800

    2022-11-14 ver2, only change version code, due to lost resource file for binary.

--- chrome_tixcraft.py
+++ chrome_tixcraft.py
@@ -67,7 +67,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 #附註1：沒有寫的很好，很多地方應該可以模組化。
 #附註2：
 
-CONST_APP_VERSION = u"MaxBot (2022.11.14)"
+CONST_APP_VERSION = u"MaxBot (2022.11.14) ver.2"
 
 CONST_FROM_TOP_TO_BOTTOM = u"from top to bottom"
 CONST_FROM_BOTTOM_TO_TOP = u"from bottom to top"
--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ import platform
 import json
 import webbrowser
 
-CONST_APP_VERSION = u"MaxBot (2022.11.14)"
+CONST_APP_VERSION = u"MaxBot (2022.11.14) ver.2"
 
 CONST_FROM_TOP_TO_BOTTOM = u"from top to bottom"
 CONST_FROM_BOTTOM_TO_TOP = u"from bottom to top"
