e917dfbf160887437ebd7131b465ec02b8ba1e30
CHUN YU YAO <weng.32002@gmail.com>
2023-05-31 20:09:59 +0800

    ver 2023-05-26

--- chrome_tixcraft.py
+++ chrome_tixcraft.py
@@ -54,7 +54,7 @@ import itertools
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-CONST_APP_VERSION = u"MaxBot (2023.05.25)"
+CONST_APP_VERSION = u"MaxBot (2023.05.26)"
 
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
 CONST_MAXBOT_LAST_URL_FILE = "MAXBOT_LAST_URL.txt"
