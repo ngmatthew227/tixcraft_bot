678ca76eb25c05cded7e4d9639e03eee52c63ff2
Max <max32002@gmail.com>
2023-06-13 11:48:14 +0800

    change version code

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ import threading
 import subprocess
 import json
 
-CONST_APP_VERSION = "MaxBot (2023.6.9)"
+CONST_APP_VERSION = "MaxBot (2023.6.10)"
 
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
 CONST_MAXBOT_LAST_URL_FILE = "MAXBOT_LAST_URL.txt"
