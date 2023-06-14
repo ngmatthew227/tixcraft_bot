305b5b1812d515f7422197530dfc13132c6f383f
Max <max32002@gmail.com>
2023-06-14 15:15:01 +0800

    change version code

--- config_launcher.py
+++ config_launcher.py
@@ -22,7 +22,7 @@ import base64
 import threading
 import subprocess
 
-CONST_APP_VERSION = "MaxBot (2023.6.12)"
+CONST_APP_VERSION = "MaxBot (2023.6.13)"
 
 CONST_MAXBOT_LAUNCHER_FILE = "config_launcher.json"
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
