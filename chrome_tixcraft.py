4db641a05d434fb3e298e210ee77bfcd96343ad0
Max <max32002@gmail.com>
2023-06-19 17:26:33 +0800

    change version code

--- chrome_tixcraft.py
+++ chrome_tixcraft.py
@@ -53,7 +53,7 @@ import webbrowser
 import argparse
 import itertools
 
-CONST_APP_VERSION = "MaxBot (2023.6.18)"
+CONST_APP_VERSION = "MaxBot (2023.6.19)"
 
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
 CONST_MAXBOT_LAST_URL_FILE = "MAXBOT_LAST_URL.txt"
