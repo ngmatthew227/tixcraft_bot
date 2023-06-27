5dc59d9be43579906b45ef4cec6beac8c52e8b4e
Max <max32002@gmail.com>
2023-06-27 10:40:10 +0800

    change version code.

--- text_server.py
+++ text_server.py
@@ -28,7 +28,7 @@ import asyncio
 import tornado
 from tornado.web import Application
 
-CONST_APP_VERSION = "MaxBot (2023.6.23)"
+CONST_APP_VERSION = "MaxBot (2023.6.24)"
     
 CONST_MAXBOT_QUESTION_FILE = "MAXBOT_QUESTION.txt"
 
