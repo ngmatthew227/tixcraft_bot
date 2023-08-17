d810cb99842fecb0d9b63aa8dc14ebb6a70b0250
Max <max32002@gmail.com>
2023-08-17 13:58:55 +0800

    change version code

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ warnings.simplefilter('ignore',InsecureRequestWarning)
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-CONST_APP_VERSION = "MaxBot (2023.08.12)"
+CONST_APP_VERSION = "MaxBot (2023.08.13)"
 
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
 CONST_MAXBOT_LAST_URL_FILE = "MAXBOT_LAST_URL.txt"
