df66d9a9d927f93dd54e349376b5f0c350c5584f
Max <max32002@gmail.com>
2023-10-02 16:46:40 +0800

    Update chrome_tixcraft.py

--- chrome_tixcraft.py
+++ chrome_tixcraft.py
@@ -55,7 +55,7 @@ import webbrowser
 
 import chromedriver_autoinstaller
 
-CONST_APP_VERSION = "MaxBot (2023.09.04)"
+CONST_APP_VERSION = "MaxBot (2023.09.05)"
 
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
 CONST_MAXBOT_LAST_URL_FILE = "MAXBOT_LAST_URL.txt"
