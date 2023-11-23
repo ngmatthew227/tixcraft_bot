e0b61402902be420245498689e21da1521a72581
Max <max32002@gmail.com>
2023-11-23 14:50:37 +0800

    change version code

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-CONST_APP_VERSION = "MaxBot (2023.11.15)"
+CONST_APP_VERSION = "MaxBot (2023.11.16)"
 
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
 CONST_MAXBOT_LAST_URL_FILE = "MAXBOT_LAST_URL.txt"
