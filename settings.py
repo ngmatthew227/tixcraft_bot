f9de28d2c50a9cc66153e1cf53a17f800f00d74d
CHUN YU YAO <weng.32002@gmail.com>
2023-12-28 22:40:01 +0800

    2023-12-17, bug fix for kktix.

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-CONST_APP_VERSION = "MaxBot (2023.12.16)"
+CONST_APP_VERSION = "MaxBot (2023.12.17)"
 
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
 CONST_MAXBOT_LAST_URL_FILE = "MAXBOT_LAST_URL.txt"
--- webdriver/Maxbot_1.0.0/js/kktix_registrations_new.js
+++ webdriver/Maxbot_1.0.0/js/kktix_registrations_new.js
@@ -2,8 +2,5 @@ $("footer").hide();
 function hideBannerAndImage() {
     $("div.banner-wrapper").hide();
     $("div.ticket-img-wrapper").hide();
-    $('input[type=checkbox]').each(function() {
-        $(this).prop('checked', true);
-    });
 }
 setTimeout(hideBannerAndImage, 300);
