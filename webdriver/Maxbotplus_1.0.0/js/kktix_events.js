0f35ce7ca167a493ce6042fde6fd2167b3e6eb15
Your Name <you@example.com>
2024-01-26 16:53:49 +0800

    allow kktix redirect with tracker url, ex: fbid

--- webdriver/Maxbotplus_1.0.0/js/kktix_events.js
+++ webdriver/Maxbotplus_1.0.0/js/kktix_events.js
@@ -6,12 +6,15 @@ $("footer").remove();
 function kktix_event_redirect()
 {
     const currentUrl = window.location.href; 
-    const event_code = currentUrl.split('/')[4];
+    let event_code = currentUrl.split('/')[4];
+    if(event_code.indexOf('?') > -1) {
+        event_code = event_code.split('?')[0];
+    }
     //console.log(currentUrl);
     //console.log(event_code);
     if(event_code){
         let button_count = $("div.tickets > a.btn-point").length;
-        console.log("length:"+button_count);
+        //console.log("length:"+button_count);
         if (button_count == 1) {
             let new_url = "https://kktix.com/events/"+ event_code +"/registrations/new";
             location.href=new_url;
