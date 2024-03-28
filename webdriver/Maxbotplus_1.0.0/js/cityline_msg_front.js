eb4f0dbeb597a2e56a3d5db13ac40a68fe7e4926
Your Name <you@example.com>
2024-03-28 13:01:54 +0800

    2024-03-12, update for cityline

--- webdriver/Maxbotplus_1.0.0/js/cityline_msg_front.js
+++ webdriver/Maxbotplus_1.0.0/js/cityline_msg_front.js
@@ -1,21 +1,72 @@
-function goEvent(){
-    if(urlThrottler.indexOf('https://event.cityline.com/')>-1) {
-        window.location.href = urlThrottler;
-    } else {
-        document.getElementById("multiple_tab_layout").innerHTML = 'start to retry';
-
-        var ddsScheduler = undefined;
-        var retryingMsg = "重試中...<br>Retrying...";
-        var retryingMsg2 = "重試中... Retrying...";
-        var enableAutoRetry = true;
-        var remainTime = 10;
-        var enableButtonTime = 7;
-        
-        document.getElementById("busy_zone").innerHTML = '<button id="btn-retry-en-1" class="btn_cta" type="button" onclick="javascript:goEvent()">請重試 Retry<span id="remainTime1"></span></button>';
-        setTimeout(startCountDownTimer, 1000);
-    }
-    
+function getHtmlDocName() {
+    var pathname = location.pathname;
+   var pathParts = pathname.split('/');
+   if(pathParts.length >= 3) return pathParts[2];
+   return null;
 }
+
+function goToCityline(){
+   window.location="https://www.cityline.com";
+}
+
+function setRetryUrl(requestUrl){
+   urlThrottler = requestUrl;
+}
+
+function startCountDownTimer() {
+   setRetryUrl(window.location.href);
+   
+   if (enableAutoRetry){
+      setRemainTime(remainTime); 
+      if (ddsScheduler == undefined) {
+         ddsScheduler = setInterval(function(){
+            updateRemainTime();
+         }, 1000);
+      }
+   }else{
+      document.getElementById("remainTime1").innerHTML = '';
+      $('#btn-retry-en-1').removeAttr('disabled');
+   }
+}
+
+function setRemainTime(sec) {
+   document.getElementById("remainTime1").innerHTML = '(' +sec+ ')';
+}
+
+function goEvent(){
+   window.location.href = urlThrottler;
+}
+
+/*
+function goEvent(){
+    if(urlThrottler) {
+        if(window.location.href.indexOf("https://msg.cityline.com/") > -1) {
+            if(urlThrottler == "https://event.cityline.com") {
+                if(window.location.href.indexOf("?") > -1) {
+                    urlThrottler = "https://event.cityline.com/queue?" + window.location.href.split("?")[1];
+                }
+                document.getElementById("multiple_tab_layout").innerHTML = '';
+                $('#busy_zone').removeClass('d-none')
+                document.getElementById("busy_zone").innerHTML = '<div class="event"><button id="btn-retry-en-1" class="btn_cta" type="button" onclick="javascript:goEvent()">請重試 Retry<span id="remainTime1"></span></button></div>';
+
+                var ddsScheduler = undefined;
+                setRemainTime(remainTime); 
+                if (ddsScheduler == undefined) {
+                 ddsScheduler = setInterval(function(){
+                    updateRemainTime();
+                 }, 1000);
+                }
+
+            }
+        }
+
+        if(urlThrottler.indexOf("?") > -1) {
+            document.getElementById("multiple_tab_layout").innerHTML = urlThrottler;
+            window.location.href = urlThrottler;
+        }
+    }
+}
+*/
 
 function begin()
 {
@@ -66,3 +117,36 @@ function dom_ready()
 myInterval = setInterval(() => {
     dom_ready();
 }, 100);
+
+var ItemType = {
+    Session: 1,
+    Local: 2
+};
+
+var localStorageTabKey = 'my-application-browser-tab';
+function SetItem(itemtype, val) {
+ switch (itemtype) {
+    case ItemType.Session:
+       window.name = val;
+       break;
+    case ItemType.Local:
+       setCookie(localStorageTabKey, val);
+       break;
+ }
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+setInterval(() => {
+    console.log('set item');
+    SetItem(ItemType.Local, "");
+}, 100);
+
