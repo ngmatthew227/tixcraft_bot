639da5a3d789e3bd5b325dd2ee697fc492398fa3
Your Name <you@example.com>
2024-04-15 10:26:09 +0800

    change version to 2024-04-04

--- config_launcher.py
+++ config_launcher.py
@@ -24,7 +24,7 @@ import webbrowser
 
 import util
 
-CONST_APP_VERSION = "MaxBot (2024.04.03)"
+CONST_APP_VERSION = "MaxBot (2024.04.04)"
 
 CONST_MAXBOT_LAUNCHER_FILE = "config_launcher.json"
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
--- nodriver_tixcraft.py
+++ nodriver_tixcraft.py
@@ -33,7 +33,7 @@ except Exception as exc:
     print(exc)
     pass
 
-CONST_APP_VERSION = "MaxBot (2024.04.03)"
+CONST_APP_VERSION = "MaxBot (2024.04.04)"
 
 CONST_MAXBOT_ANSWER_ONLINE_FILE = "MAXBOT_ONLINE_ANSWER.txt"
 CONST_MAXBOT_CONFIG_FILE = "settings.json"
